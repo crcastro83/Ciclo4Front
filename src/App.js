5dba89b341b2cd4a5705bdfbbf55e6d4b9b32e66
Carlos <carlos.castro.mt@usa.edu.co>
2022-12-03 19:16:55 -0500

    Se anexa segundo avance del frontEnd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Admin from './componentes/Admin';
+import CrearCuenta from './componentes/CrearCuenta';
+import Home from './componentes/Home';
+import Login from './componentes/login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home/>} />
+        <Route path="/login" exact element={<Login/>} />
+        <Route path="/crear-cuenta" exact element={<CrearCuenta/>} />
+        <Route path="/admin" exact element={<Admin/>} />
+      </Routes>
+    </Router>
   );
 }
 
